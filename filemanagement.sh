88bfe10b081f158e34fa08a4cca424908868da8f
FlevianK <flevian.kanaiza@andela.com>
2018-01-15 14:32:50 +0300

    bash scripting files

---
+++ filemanagement.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir helloword
+mkdir -p hello/world
+mkdir -p trail/one
+mv trail hellword
+cd helloword touch hello.sh
+cd helloword touch morning.sh 
+pushd /helloword/morning.sh
+    cat <<EOF > morning.sh
+  echo "hello their"
+  EOF
+popd
+
